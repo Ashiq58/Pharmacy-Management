f99494206330d6df3f220826af971c48b5bfc3cd
Ashiq58 <ashikul58bd@gmail.com>
2024-05-19 17:11:38 +0600

    Update

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,42 @@
 <template>
   <LoginPanel></LoginPanel>
+<div class="toasts">
+  <TheToast></TheToast>
+</div>
 </template>
 
 <script>
 import LoginPanel from "./components/LoginPanel.vue";
+import TheToast from "./components/TheToast.vue";
 export default {
-
+  data: () => ({
+    toasts: [
+      // {
+      //   type: "Success",
+      //   message: "Done Successfully!"
+      // },
+      // {
+      //   type: "Error",
+      //   message: "Something went wrong!"
+      // }
+    ]
+  }),
   components: {
     LoginPanel,
+    TheToast
   }
 }
 </script>
 
-<style></style>
+<style>
+.toasts {
+  position: absolute;
+  top: 0;
+  right: 0;
+  z-index: 9999999;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+  overflow-x: hidden;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -2,8 +2,14 @@ import { createApp } from 'vue'
 
 import App from './App.vue'
 
-import "./assets/css/bootstrap.css"
-import "./assets/js/jquery-3.7.1.min.js"
-import "./assets/js/bootstrap.bundle.js"
+import "./assets/css/ayoflex.css";
+// import "ayoflex/dist/ayoflex.min.css";
+
+import "./assets/css/example.css";
+// import "bootstrap/dist/css/bootstrap.min.css"
+// import "bootstrap"
+// import "./assets/css/bootstrap.css"
+// import "./assets/js/jquery-3.7.1.min.js"
+// import "./assets/js/bootstrap.bundle.js"
 
 createApp(App).mount('#app')
